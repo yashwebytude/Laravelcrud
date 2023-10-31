50975992f4622410b3946147de92feef1b6526fb
PHP1 <php1.webytude@gmail.com>
2023-10-31 13:08:43 +0530

    add validation message in blade file

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -6,6 +6,15 @@
     <title>Create</title>
 </head>
 <body>
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
     <form action="{{route('store')}}" method="post">
     @csrf
         <div>
